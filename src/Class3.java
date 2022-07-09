e7c3f7fa8306c9bbdf3a340a6b28890587adb2df
luuquangict <luuquangict@gmail.com>
2022-07-09 15:55:14 +0700

    Add class 3 from other

---
+++ src/Class3.java
@@ -0,0 +1,12 @@
+public class Class2 {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
