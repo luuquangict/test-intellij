7fd926254b832fe63c009fbc55aa18f858684a29
luuquangict <luuquangict@gmail.com>
2022-07-09 15:39:23 +0700

    Init

---
+++ src/Class1.java
@@ -0,0 +1,55 @@
+public class Class1 {
+    public static void main(String[] args) {
+
+        int ChuSoThapPhan;
+        int chuSoThapPhan;
+
+        // số
+        // chữ cái: a-z (phân biệt hoa thường)
+        // dấu _
+
+        // int => 4 bytes
+        // 3.14 => double
+        // 1.0 => double => 8 bytes
+
+        // số nguyên
+        // số thực
+        // Số nguyên dương
+        // số nguyên âm
+        // Văn bản
+        // đúng hay sai
+
+        // int => số nguyên
+        // float, double => số thực
+
+        // boolean/bool => đúng hay sai
+        // String => văn bản => chuỗi "Nguyễn Văn A"
+        // char 'a', 'b', 'c' '@'
+        // => Kiểu dữ liệu cơ sở
+
+        // lưu năm sinh của một người
+        // Tùng: int
+        // Hải: Số thực double
+        // Bách: short, int
+        //
+
+        int x;
+        x = 100;
+
+        int y = 100;
+
+        y = 1000;
+        y = 3;
+        y = 5;
+
+        final double pi;
+        pi = 1;
+
+        // đúng: true => bóng đèn được bật
+        // sai: false => bóng đèn tắt
+
+        System.out.println("hello world3");
+        System.out.println("hello world3");
+        System.out.println("hello world3");
+    }
+}
